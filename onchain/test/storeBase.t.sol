af40b3f88d7dd911d318d4641b49c170fc9d0976
OkoliEvans <ebounce500@gmail.com>
2024-08-15 11:02:36 +0100

    first commit: init

---
+++ onchain/test/storeBase.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Store} from "../src/storeBase.sol";
+
+contract StoreTest is Test {
+    Store public store;
+
+   
+}
