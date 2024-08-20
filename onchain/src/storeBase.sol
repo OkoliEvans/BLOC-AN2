40b26bcdd5e786681b087d55780f4ccfc684ed77
OkoliEvans <ebounce500@gmail.com>
2024-08-20 18:37:32 +0100

    edit the createUserAccount function. Added mapping isRegistered to track if a user has registered prior, added registeredUSers array to hold registered addresses, added userIdAssigned to track total users registered, and totalBuys to track total purchases in USD

--- onchain/src/storeBase.sol
+++ onchain/src/storeBase.sol
@@ -2,21 +2,28 @@
 pragma solidity ^0.8.13;
 
 contract Store {
+	uint256 userIdAssigned;
+	uint256 totalBuys;
 
 	// Define the user struct
 	struct User {
+		uint256 id;
 		string shippingAddress;
-		uint8 totalBuys;
+		uint256 totalBuys;
 	}
 
 	// Mapping to store user data based on their address
 	mapping(address => User) public users;
+	mapping(address => bool) private isRegistered;
+	address[] registeredUsers;
 
 	// create a new user account
 	function createUserAccount(string memory shippingAddress) public {
-		require(!users[msg.sender].shippingAddress, "Account already exists");
+		uint256 userId = userIdAssigned += 1;
 		require(bytes(shippingAddress).length > 0, "Shipping Address cannot be empty");
-
-		users[msg.sender] = User({shippingAddress, totalBuys: 0});
+		require(isRegistered[msg.sender] == false, "user cannot register same address twice");
+		users[msg.sender] = User({id: userId, shippingAddress: shippingAddress, totalBuys: 0});
+		registeredUsers.push(msg.sender);
+		isRegistered[msg.sender] = true;
 	}
 }
