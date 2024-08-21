34ff386148ec1ac66b2faa983df838835c30689e
GideonBature <infoaboutgideon@gmail.com>
2024-08-21 02:29:52 +0100

    update test to accommodate for user id

--- onchain/test/storeBase.t.sol
+++ onchain/test/storeBase.t.sol
@@ -16,11 +16,13 @@ contract StoreTest is Test {
 
         store.createUserAccount(shippingAddress);
 
-        (string memory storedShippingAddress, uint8 totalBuys) = store.users(address(this));
+        (uint256 id, string memory storedShippingAddress, uint256 totalBuys) = store.users(address(this));
 
         assertEq(storedShippingAddress, shippingAddress, "Shipping address should match");
 
         assertEq(totalBuys, 0, "Total buys should be initialized to 0.");
+
+        assertGt(id, 0, "User ID should be greater than 0.");
     }
 
     function testCreateUserAccountWithEmptyAddress() public {
@@ -37,6 +39,21 @@ contract StoreTest is Test {
 
         vm.expectRevert("Account already exists");
         store.createUserAccount(shippingAddress);
+    }
+
+    function testUserIdIncrement() public {
+        string memory shippingAddress_1 = "123 Main Street, City, Country";
+        string memory shippingAddress_2 = "456 Another Street, City, Country";
+
+        store.createUserAccount(shippingAddress_1);
+        uint256 firstUserId = store.users(address(this)).id;
+
+        address secondUser = address(0x123456);
+        vm.prank(secondUser);
+        store.createUserAccount(shippingAddress_2);
+        uint256 secondUserId = store.users(secondUser).id;
+
+        assertEq(firstUserId, secondUserId - 1, "User ID should be incremented by 1.");
     }
 
    
